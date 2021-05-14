17a93dd011e3a0c3c824906867a3c948c80a28b8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 04:45:09 +0900

    Peerのイベントを受信するクラス

---
+++ Client/PhotonServerTestClient/Assets/Scripts/PeerEventListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+public class PeerEventListener : MonoBehaviour, IPhotonPeerListener
+{
+    public void DebugReturn(DebugLevel level, string message)
+    {
+    }
+
+    public void OnEvent(EventData eventData)
+    {
+    }
+
+    public void OnOperationResponse(OperationResponse operationResponse)
+    {
+    }
+
+    public void OnStatusChanged(StatusCode statusCode)
+    {
+    }
+}
