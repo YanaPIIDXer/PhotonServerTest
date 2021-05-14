36da39601dd303a156fab090bf1e233ac016005a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 07:34:17 +0900

    イベント、レスポンス送信メソッド定義

--- Server/GamePeer.cs
+++ Server/GamePeer.cs
@@ -1,5 +1,6 @@
 using Photon.SocketServer;
 using PhotonHostRuntimeInterfaces;
+using Common.Packet;
 
 public class GamePeer : ClientPeer
 {
@@ -14,4 +15,27 @@ public class GamePeer : ClientPeer
     protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
     {
     }
-}
+
+    /// <summary>
+    /// イベントパケット送信
+    /// </summary>
+    /// <param name="Packet">パケット</param>
+    public void SendEventPacket(EventPacket Packet)
+    {
+        byte Code = Packet.SendCode;
+        var Params = Packet.SendParamsDictionary;
+        var Data = new EventData(Code, Params);
+        SendEvent(Data, new SendParameters());
+    }
+
+    /// <summary>
+    /// レスポンスパケット送信
+    /// </summary>
+    /// <param name="Packet">パケット</param>
+    public void SendResponsePacket(OperationPacket Packet)
+    {
+        byte Code = Packet.SendCode;
+        var Params = Packet.SendParamsDictionary;
+        var Response = new OperationResponse(Code, Params);
+        SendOperationResponse(Response, new SendParameters());
+    }
