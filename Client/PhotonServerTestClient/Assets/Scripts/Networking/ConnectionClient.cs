77fe76d39b0d429e89a81704d2db7161ac63fa1f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 05:24:06 +0900

    サーバと接続するクラス

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Networking/ConnectionClient.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+namespace Game.Networking
+{
+    /// <summary>
+    /// 接続クライアント
+    /// </summary>
+    public class ConnectionClient : MonoBehaviour, IPhotonPeerListener
+    {
+        /// <summary>
+        /// Peer
+        /// </summary>
+        private PhotonPeer Peer = null;
+
+        #region Singleton
+        public static ConnectionClient Instance { get { return _Instance; } }
+        private static ConnectionClient _Instance = null;
+        #endregion
+
+        void Awake()
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            _Instance = this;
+        }
+
+        void Update()
+        {
+            if (Peer != null)
+            {
+                Peer.Service();
+            }
+        }
+
+        /// <summary>
+        /// 接続
+        /// </summary>
+        public void Connect()
+        {
+            if (Peer != null) { return; }
+
+            Peer = new PhotonPeer(this, ConnectionProtocol.Tcp);
+            Peer.Connect("127.0.0.1", "TestServer");
+        }
+
+        public void DebugReturn(DebugLevel level, string message)
+        {
+            string Message = string.Format("Level:{0} {1}", level.ToString(), message);
+            if (level != DebugLevel.ERROR)
+            {
+                Debug.Log(Message);
+            }
+            else
+            {
+                Debug.LogError(Message);
+            }
+        }
+
+        public void OnEvent(EventData eventData)
+        {
+        }
+
+        public void OnOperationResponse(OperationResponse operationResponse)
+        {
+        }
+
+        public void OnStatusChanged(StatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case StatusCode.Connect:
+
+                    DebugReturn(DebugLevel.INFO, "Connection Success!!");
+                    break;
+            }
+        }
+    }
+}
