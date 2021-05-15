d3c5c56c2664775d869c2af06fd9a82ea3439b71
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 05:39:18 +0900

    UIManagerのPrefab化

---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// UI管理
+    /// </summary>
+    public class UIManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Prefabのパス
+        /// </summary>
+        private static readonly string PrefabPath = "Prefabs/System/UIManager";
+
+        /// <summary>
+        /// 背面キャンバスのトランスフォーム
+        /// </summary>
+        [SerializeField]
+        private Transform BackCanvasTransform = null;
+
+        /// <summary>
+        /// 中間キャンバスのトランスフォーム
+        /// </summary>
+        [SerializeField]
+        private Transform MiddleCanvasTransform = null;
+
+        /// <summary>
+        /// 前面キャンバスのトランスフォーム
+        /// </summary>
+        [SerializeField]
+        private Transform FrontCanvasTransform = null;
+
+        #region Singleton
+        public static UIManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    GameObject Prefab = Resources.Load<GameObject>(PrefabPath);
+                    Debug.Assert(Prefab != null, "UIManager Prefab Load Failed.");
+
+                    GameObject Obj = Instantiate<GameObject>(Prefab, Vector3.zero, Quaternion.identity);
+                    Debug.Assert(Obj != null, "UIManager Instantiate Failed.");
+
+                    _Instance = Obj.GetComponent<UIManager>();
+                }
+                return _Instance;
+            }
+        }
+        private static UIManager _Instance = null;
+        #endregion
+    }
+}
