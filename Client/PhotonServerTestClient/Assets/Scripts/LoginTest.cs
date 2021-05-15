7cc5f8b829130e3711bfc0af4f4f08f166656279
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 09:04:05 +0900

    ログイン処理の実験用 ※クライアント側のアーキテクチャがまだ未定なので使い捨て

---
+++ Client/PhotonServerTestClient/Assets/Scripts/LoginTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Networking;
+using System;
+using UniRx;
+using ExitGames.Client.Photon;
+using Common.Code;
+
+/// <summary>
+/// ログイン実験用
+/// ※クライアント側のアーキテクチャがまだ定まっていないので破棄前提
+/// </summary>
+public class LoginTest : MonoBehaviour
+{
+    void Awake()
+    {
+        ConnectionClient.OnConnectionStatusChanged
+            .Where((Code) => Code == StatusCode.Connect)
+            .Subscribe((_) =>
+            {
+            }).AddTo(gameObject);
+
+        ConnectionClient.OnRecvResponse
+            .Where((Packet) => Packet.Code == EOperationCode.LogIn)
+            .Subscribe((_) => Debug.Log("LogIn Success!!"))
+            .AddTo(gameObject);
+    }
+}
