1eeb6ecfec247d256fce25efe242b5b4f7483a30
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 06:49:39 +0900

    MonoBehaviourを使わないPureなクラスのComponentの仕組みを実装

--- Client/PhotonServerTestClient/Assets/Scripts/Character/Character.cs
+++ Client/PhotonServerTestClient/Assets/Scripts/Character/Character.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game.Character.Component;
 
 namespace Game.Character
 {
@@ -9,5 +10,36 @@ namespace Game.Character
     /// </summary>
     public abstract class Character : MonoBehaviour
     {
+        /// <summary>
+        /// コンポーネントリスト
+        /// </summary>
+        private List<CharacterComponent> Components = new List<CharacterComponent>();
+
+        /// <summary>
+        /// キャラクタコンポーネントを追加
+        /// </summary>
+        /// <param name="Cmp">キャラクタコンポーネント</param>
+        protected void AddCharacterComponent(CharacterComponent Cmp)
+        {
+            Cmp.Initialize(this);
+            Components.Add(Cmp);
+        }
+
+        void Update()
+        {
+            foreach (var Cmp in Components)
+            {
+                Cmp.OnUpdate();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            foreach (var Cmp in Components)
+            {
+                Cmp.OnFixedUpdate();
+            }
+        }
+
     }
 }
---
+++ Client/PhotonServerTestClient/Assets/Scripts/Character/Component/CharacterComponent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Character.Component
+{
+    /// <summary>
+    /// キャラクタコンポーネント基底クラス
+    /// </summary>
+    public abstract class CharacterComponent
+    {
+        /// <summary>
+        /// 所有者
+        /// </summary>
+        private Character Owner = null;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CharacterComponent()
+        {
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <param name="Owner">所有者</param>
+        public void Initialize(Character Owner)
+        {
+            this.Owner = Owner;
+            OnInitialize();
+        }
+
+        /// <summary>
+        /// MonoBehaviour派生Component取得
+        /// </summary>
+        /// <typeparam name="T">Componentの型</typeparam>
+        /// <returns>Component</returns>
+        protected T GetMonoBehaviourComponent<T>()
+        {
+            return Owner.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// 初期化された
+        /// </summary>
+        protected virtual void OnInitialize() { }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public virtual void OnUpdate() { }
+
+        /// <summary>
+        /// FixedUpdate
+        /// </summary>
+        public virtual void OnFixedUpdate() { }
+    }
+}
