c1af8b205dcbba534dfe19efee3e0b56024a3e74
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 06:28:16 +0900

    とりあえずバーチャルスティックを出してみた

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Sequence/GameSequence.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.UI;
+using System;
+using UniRx;
+
+namespace Game.Sequence
+{
+    /// <summary>
+    /// ゲームシーケンス
+    /// </summary>
+    public class GameSequence : MonoBehaviour
+    {
+        void Awake()
+        {
+            var Handler = UIManager.Show<ControlStick>("Game/PlayerMoveInput", ECanvas.Middle);
+            Handler.UIComponent.OnInput.Subscribe((Value) => Debug.Log(Value));
+        }
+    }
+}
