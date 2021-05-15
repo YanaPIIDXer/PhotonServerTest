8616be3ab092aaa45f19afe655c3f4b337795137
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 06:39:43 +0900

    キャラクタ基底クラスとプレイヤークラスを定義し、プレイヤーのPrefabにプレイヤークラスをAttachした

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Character
+{
+    /// <summary>
+    /// キャラクタクラス
+    /// </summary>
+    public abstract class Character : MonoBehaviour
+    {
+    }
+}
