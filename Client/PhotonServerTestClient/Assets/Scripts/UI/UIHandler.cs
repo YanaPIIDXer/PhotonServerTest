c189737c90018a87aa5f4e82ef713eafcf95bd94
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 06:18:24 +0900

    別にインナークラスにする理由もないので別ソースに移動

---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// UIハンドラ
+    /// </summary>
+    public class UIHandler<T>
+        where T : MonoBehaviour
+    {
+        /// <summary>
+        /// UIのGameObject
+        /// </summary>
+        private GameObject UIObject = null;
+
+        /// <summary>
+        /// UIComponent
+        /// </summary>
+        public T UIComponent { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="UIObject">UIのGameObject</param>
+        /// <param name="ParentTransform">親のTransform</param>
+        /// <param name="OriginPos">元々の座標</param>
+        public UIHandler(GameObject UIObject, Transform ParentTransform, Vector3 OriginPos)
+        {
+            this.UIObject = UIObject;
+            this.UIObject.transform.SetParent(ParentTransform);
+            UIComponent = this.UIObject.GetComponent<T>();
+            this.UIObject.transform.localPosition = OriginPos;
+        }
+
+        /// <summary>
+        /// 破棄
+        /// </summary>
+        public void Destroy()
+        {
+            GameObject.Destroy(UIObject);
+        }
+    }
+}
--- Client/PhotonServerTestClient/Assets/Scripts/UI/UIManager.cs
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/UIManager.cs
@@ -48,45 +48,6 @@ namespace Game.UI
         private Transform FrontCanvasTransform = null;
 
         /// <summary>
-        /// UIハンドラ
-        /// </summary>
-        public class UIHandler<T>
-            where T : MonoBehaviour
-        {
-            /// <summary>
-            /// UIのGameObject
-            /// </summary>
-            private GameObject UIObject = null;
-
-            /// <summary>
-            /// UIComponent
-            /// </summary>
-            public T UIComponent { get; private set; }
-
-            /// <summary>
-            /// コンストラクタ
-            /// </summary>
-            /// <param name="UIObject">UIのGameObject</param>
-            /// <param name="ParentTransform">親のTransform</param>
-            /// <param name="OriginPos">元々の座標</param>
-            public UIHandler(GameObject UIObject, Transform ParentTransform, Vector3 OriginPos)
-            {
-                this.UIObject = UIObject;
-                this.UIObject.transform.SetParent(ParentTransform);
-                UIComponent = this.UIObject.GetComponent<T>();
-                this.UIObject.transform.localPosition = OriginPos;
-            }
-
-            /// <summary>
-            /// 破棄
-            /// </summary>
-            public void Destroy()
-            {
-                GameObject.Destroy(UIObject);
-            }
-        }
-
-        /// <summary>
         /// 表示
         /// </summary>
         /// <param name="PrefabRelativePath">Preabs/UI/からのPrefab相対パス</param>
