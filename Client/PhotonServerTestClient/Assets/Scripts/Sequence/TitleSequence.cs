78a80b9f22941dafebdda31ecd8b7bf6d9982e21
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 10:07:14 +0900

    タイトル画面シーケンスクラス ログイン後にゲーム画面に飛ばすようにした

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Sequence/TitleSequence.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Networking;
+using System;
+using UniRx;
+using Common.Code;
+using Common.Packet;
+using UnityEngine.SceneManagement;
+
+namespace Game.Sequence
+{
+    /// <summary>
+    /// タイトル画面シーケンス
+    /// </summary>
+    public class TitleSequence : MonoBehaviour
+    {
+        void Awake()
+        {
+            ConnectionClient.OnConnectionStatusChanged
+                .Where((Code) => Code == ExitGames.Client.Photon.StatusCode.Connect)
+                .Subscribe((_) =>
+                {
+                    OperationPacket Packet = new OperationPacket(EOperationCode.LogIn, new Dictionary<byte, object>());
+                    ConnectionClient.Instance.SendRequest(Packet);
+                }).AddTo(gameObject);
+
+            ConnectionClient.OnRecvResponse
+                .Where((Packet) => Packet.Code == EOperationCode.LogIn)
+                .Subscribe((Packet) =>
+                {
+                    SceneManager.LoadScene("Game");
+                }).AddTo(gameObject);
+        }
+    }
+}
