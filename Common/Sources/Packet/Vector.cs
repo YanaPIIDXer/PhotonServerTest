f2c04eceded87ae629ee61c288e8b278fcdb4dee
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 09:34:34 +0900

    シリアライズ用のベクトル構造体定義

---
+++ Common/Sources/Packet/Vector.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Common.Packet
+{
+    /// <summary>
+    /// ３次元ベクトル
+    /// </summary>
+    [Serializable]
+    public struct Vec3
+    {
+        /// <summary>
+        /// X
+        /// </summary>
+        public float X;
+
+        /// <summary>
+        /// Y
+        /// </summary>
+        public float Y;
+
+        /// <summary>
+        /// Z
+        /// </summary>
+        public float Z;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="X">X</param>
+        /// <param name="Y">Y</param>
+        /// <param name="Z">Z</param>
+        public Vec3(float X = 0, float Y = 0, float Z = 0)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+}
