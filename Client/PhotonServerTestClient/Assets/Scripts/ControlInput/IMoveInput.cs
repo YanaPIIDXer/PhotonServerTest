3b9320ec194f7584dec7f481d7ef701a54af9868
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 17:05:25 +0900

    移動入力用インタフェース定義

---
+++ Client/PhotonServerTestClient/Assets/Scripts/ControlInput/IMoveInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+namespace Game.ControlInput
+{
+    /// <summary>
+    /// 移動入力インタフェース
+    /// </summary>
+    public interface IMoveInput
+    {
+        /// <summary>
+        /// 移動入力
+        /// </summary>
+        IObservable<Vector2> OnInputMove { get; }
+    }
+}
