dfc3e51d6d23b8f34fa7053748ac3b92049be219
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 16:22:49 +0900

    プレイヤークラスをとりあえず定義

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Character/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Character.Player
+{
+    /// <summary>
+    /// プレイヤークラス
+    /// </summary>
+    public class Player : Character
+    {
+    }
+}
