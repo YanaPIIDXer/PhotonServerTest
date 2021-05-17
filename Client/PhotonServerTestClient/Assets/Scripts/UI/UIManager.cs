c934a55d02c8b49149f496d902b57bea622c7bb2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 12:11:37 +0900

    UIマネージャをとりあえず定義

---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.System;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// UIマネージャ
+    /// </summary>
+    public class UIManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Prefabのパス
+        /// </summary>
+        private static readonly string PrefabPath = "Prefabs/System/UIManager";
+
+        /// <summary>
+        /// UIのPrefabが格納されているルートパス
+        /// </summary>
+        private static readonly string UIPrefabPathRoot = "Prefabs/UI/";
+
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static UIManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = PrefabManager.Instance.Load<UIManager>(PrefabPath);
+                }
+                return _Instance;
+            }
+        }
+        private static UIManager _Instance = null;
+
+        /// <summary>
+        /// CanvasのTransform
+        /// </summary>
+        [SerializeField]
+        private Transform CanvasTransform = null;
+    }
+}
