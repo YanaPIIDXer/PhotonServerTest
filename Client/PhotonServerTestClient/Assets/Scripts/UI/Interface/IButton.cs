eb56f7a0187110856a70b3281d4b9e85dbbc3978
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 12:51:34 +0900

    インタフェース色々

---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/Interface/IButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+
+namespace Game.UI.Interface
+{
+    /// <summary>
+    /// ボタンインタフェース
+    /// </summary>
+    public interface IButton
+    {
+        /// <summary>
+        /// 押された
+        /// </summary>
+        IObservable<Unit> OnPress { get; }
+    }
+}
---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/Interface/ISelection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+
+namespace Game.UI.Interface
+{
+    /// <summary>
+    /// 選択肢インタフェース
+    /// </summary>
+    /// <typeparam name="T">選択するものの型</typeparam>
+    public interface ISelection<T>
+    {
+        /// <summary>
+        /// 選択された
+        /// </summary>
+        IObservable<T> OnSelected { get; }
+    }
+}
