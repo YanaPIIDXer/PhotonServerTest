0815e97ba57eac397daa902695f588dc1c123f14
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 14:40:53 +0900

    ゲームシーンとそのシーケンスコンポーネント

---
+++ Client/PhotonServerTestClient/Assets/Scripts/Sequence/GameSequence.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Sequence
+{
+    /// <summary>
+    /// ゲーム画面シーケンス
+    /// </summary>
+    public class GameSequence : MonoBehaviour
+    {
+        void Awake()
+        {
+        }
+    }
+}
