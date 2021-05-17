778ad7af901c5753aca252f9164e7b0acba26107
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 12:23:16 +0900

    UIハンドラ

---
+++ Client/PhotonServerTestClient/Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// UIハンドラ
+    /// </summary>
+    /// <typeparam name="T">UIComponentの型</typeparam>
+    public class UIHandler<T>
+        where T : MonoBehaviour
+    {
+        /// <summary>
+        /// UIインスタンス
+        /// </summary>
+        public T Instance { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Instance">インスタンス</param>
+        public UIHandler(T Instance)
+        {
+            this.Instance = Instance;
+        }
+
+        /// <summary>
+        /// 破棄
+        /// </summary>
+        public void Destroy()
+        {
+            GameObject.Destroy(Instance.gameObject);
+            Instance = null;
+        }
+    }
+}
