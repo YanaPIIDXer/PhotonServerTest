a7d48807bf654da5cf8f4b629d5e91567fc8079a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-17 14:27:21 +0900

    クライアント側の準備完了パケットのレスポンスとしてサーバ側の準備完了パケット追加 ※自キャラのキャラクタＩＤ通知を目的としている

--- Common/Sources/Packet/PacketID.cs
+++ Common/Sources/Packet/PacketID.cs
@@ -11,29 +11,33 @@
 		/// </summary>
 		LogInResult = 0x02,
 		/// <summary>
-		/// 準備完了パケット
+		/// クライアント準備完了パケット
 		/// </summary>
 		ClientReady = 0x03,
 		/// <summary>
+		/// サーバ準備完了パケット
+		/// </summary>
+		ServerReady = 0x04,
+		/// <summary>
 		/// プレイヤーリストパケット
 		/// </summary>
-		PlayerList = 0x04,
+		PlayerList = 0x05,
 		/// <summary>
 		/// プレイヤー登場パケット
 		/// </summary>
-		PlayerEnter = 0x05,
+		PlayerEnter = 0x06,
 		/// <summary>
 		/// プレイヤー移動パケット
 		/// </summary>
-		PlayerMove = 0x06,
+		PlayerMove = 0x07,
 		/// <summary>
 		/// 他人移動パケット
 		/// </summary>
-		OtherPlayerMove = 0x07,
+		OtherPlayerMove = 0x08,
 		/// <summary>
 		/// プレイヤー離脱パケット
 		/// </summary>
-		PlayerLeave = 0x08,
+		PlayerLeave = 0x09,
 		
 	}
 }
---
+++ Common/Sources/Packet/PacketServerReady.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Common.Stream;
+
+namespace Common.Packet
+{
+    public class PacketServerReady  : IPacket
+	{
+		
+
+		/// <summary>
+		///  キャラクタID
+		/// </summary>
+		public int CharacterId = new int();
+
+		
+
+		public EPacketID PacketID { get { return EPacketID.ServerReady; } }
+
+		public PacketServerReady()
+		{
+		}
+
+		public PacketServerReady(int CharacterId)
+		{
+			this.CharacterId = CharacterId;
+			
+		}
+
+		public bool Serialize(IStream Stream)
+		{
+			Stream.Serialize(ref CharacterId);
+			
+			return true;
+		}
+
+		public static byte[] SerializeObject(object customType)
+		{
+			var Stream = new MemoryStreamWriter();
+			var Obj = (PacketServerReady)customType;
+			Obj.Serialize(Stream);
+			return Stream.Buffer.ToArray();
+		}
+
+		public static object DeserializeObject(byte[] data)
+		{
+			var Stream = new MemoryStreamReader(data);
+			var Obj = new PacketServerReady();
+			Obj.Serialize(Stream);
+			return Obj;
+		}
+	}
+}
